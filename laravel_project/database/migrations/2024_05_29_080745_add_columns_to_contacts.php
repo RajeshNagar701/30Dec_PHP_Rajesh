918cc48e699518b827de4e3e9c4093119306567f
GauravGoswami12967 <gaurav12967@gmail.com>
2024-05-29 13:52:52 +0530

    first commit

--- laravel_project/database/migrations/2024_05_29_080745_add_columns_to_contacts.php
+++ laravel_project/database/migrations/2024_05_29_080745_add_columns_to_contacts.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->after('id', function ($table) {
+                $table->string('name')->nullable();
+                $table->string('email')->nullable();
+                $table->string('comment')->nullable();
+            });
         });
     }
 
@@ -31,6 +29,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::table('contacts', function (Blueprint $table) {
+            //
+        });
     }
 };
--- laravel_project/database/migrations/2024_05_29_081008_add_columns_to_categories.php
+++ laravel_project/database/migrations/2024_05_29_081008_add_columns_to_categories.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('categories', function (Blueprint $table) {
+             $table->after('id', function ($table) {
+                $table->string('cate_name')->nullable();
+                $table->string('cate_img')->nullable();
+            });
         });
     }
 
@@ -27,6 +28,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('categories', function (Blueprint $table) {
+            //
+        });
     }
 };
